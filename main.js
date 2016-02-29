5fc337ac98ae7be8132b1619b46492f049bd01db
muhamd sohaib <msohaib980@gmail.com>
2016-02-28 21:43:32 -0800

    Did it agaijn from scratch

--- main.js
+++ main.js
@@ -1,62 +1,90 @@
-var array=[0];
-var random = Math.ceil(Math.random()*9)
-var newStars="<div class='star'><img src='http://i.imgur.com/kXH65I7.png'></div"
+// 'use strict';
+var array = [];
+var counter = 0;
+var limit = 5;
 
+
+
+
+var random = Math.ceil(Math.random() * 9);
+var newStars = "<div class='star'><img src='http://i.imgur.com/kXH65I7.png'></div>";
 
 $(document).ready(init);
+
 function init(){
-	
+	// for (var i = 0; i < random; i++){
+	// 	$('#starbox').append(newStars);
+	// 	// Create Event Handler for selectNumbers Function		
+	// }
 
-    for(var i=0;i<random;i++){	
-		$('#starbox').append(newStars)
-    }   	
 
-    $('.number').click(selection);
-    // ID function for confirm button
-    $('#decision').click(result);
-    // ID function for reroll button
-    $('#scramble').click(rollItAgain)
-    // ID function for play agian button
-    $('#play').click(playItAgain)
 
+	$('.numbers').click(sumNumbers);
+	$('#checked').click(confirm);
+	$('.numbers').click(toggleButton);
+	$('#playagain').click(playItAgain);
+	$('#reroll').click(rollItAgain);
+	randomFunction()
 
 }
 
 
 
-function selection(){
-	var num = $(this).text();
-	array.push(num);
+function randomFunction(){
+	$('#starbox').empty();
+	random = Math.ceil(Math.random() * 9);
+	for (var i = 0; i < random; i++){
+		$('#starbox').append(newStars);
+	}
+}	
 
-	// sum global variable
-	 sum= array.reduce(function(b,a){
-		return Number(b)+Number(a)
+//selectNumbers function
+function sumNumbers(){
+	var num  = $(this).text();
+	// $(this).css('background-color', 'red');
+	array.push(parseFloat(num));
+	// console.log(array);
+	sum = array.reduce(function(a, b){
+		return parseFloat(a) + parseFloat(b);
 	});
+	console.log('sum: ', sum);
+}
+
+
+function toggleButton(){
+ 
+	$(this).toggleClass('select');
 
 }
 
-function result(){
-	console.log(sum)
-	if (sum===random){
-		$("#displayboard").append('you got it')
-		
-	}else{
-		// console.log('not working')
-		$("#displayboard").append('next time')
+function confirm(){
+	console.log('sum: ', sum);
+	console.log('random: ', random);
+	if (sum === random){
+		$('#displayResult').text("Correct!");
+		// $('.select').css('display', 'none');
+	} else {
+		$('#displayResult').text("Wrong, try again!!");
 	}
 
+	$('.select').remove();
 }
 
-// 
+
+
+
 function rollItAgain(){
-  
-		$('.star').remove();
-		
+	array =[];
 
+ 	if(counter < limit){
+		randomFunction();
+		counter++;
+	} else {
+		$('#reroll').attr('disabled', 'disabled');		
+	}			
 }
 
-function playItAgain( ){
-	console.log('testing')
+function playItAgain(){
+
 	location.reload();
 }
-
